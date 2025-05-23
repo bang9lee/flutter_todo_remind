0c39a75d9e7d46959d190c2e2cd9512197994ecd
bang9lee <bang9lee@gmail.com>
2025-05-23 20:44:47 +0900

    docs : "파이어베이스 구성완료"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_remind/firebase_options.dart';
 import 'package:flutter_todo_remind/ui/home/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
